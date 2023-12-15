25d8c5e9a490f6c12541f4cdbda375a20aa9f402
soorajaniyeri <aniyerisooraj@gmail.com>
2023-12-15 16:01:20 +0530

    updated

--- lib/views/userprofile.dart
+++ lib/views/userprofile.dart
@@ -1,17 +1,89 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:foodmanagement/models/usersmodel.dart';
+import 'package:http/http.dart';
 
 class UserPage extends StatelessWidget {
   const UserPage({super.key});
 
+  Future<List<UsersModel>> showData() async {
+    Response res =
+        await get(Uri.parse("https://jsonplaceholder.typicode.com/users"));
+    try {
+      if (res.statusCode == 200) {
+        List<dynamic> data = jsonDecode(res.body);
+        List<UsersModel> userList =
+            data.map((user) => UsersModel.fromJson(user)).toList();
+        return userList;
+      } else {
+        throw 'failed to load data${res.statusCode}';
+      }
+    } catch (error) {
+      throw 'failed to load data';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
-        title: const Text("Profile",style: TextStyle(color: Colors.white),),
+        title: const Text(
+          "Profile",
+          style: TextStyle(color: Colors.white),
+        ),
         centerTitle: true,
       ),
+      body: FutureBuilder(
+        future: showData(),
+        builder: (context, AsyncSnapshot<List<UsersModel>> snapshot) {
 
+          if(snapshot.connectionState==ConnectionState.waiting){
+
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                UsersModel user = snapshot.data![index];
+
+                return Card(
+                  child: ListTile(
+                    leading: const CircleAvatar(
+                      backgroundColor: Colors.blue,
+                      child: Icon(
+                        Icons.verified_user,
+                        color: Colors.white,
+                      ),
+                    ),
+                    title: Text(user.name!),
+                    subtitle: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Text(user.email!),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text(user.phone!),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          } else {
+            return const Text("No data Found");
+          }
+        },
+      ),
     );
   }
 }
